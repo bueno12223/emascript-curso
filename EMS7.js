de4d0f34c8733c908f1c5bec63c3bcadc33eb852
Jesus Berrio <berriojesus122@gmail.com>
2020-07-24 13:07:04 -0400

    EMS7

---
+++ EMS7.js
@@ -0,0 +1,11 @@
+let num = [1,2,3,4,5];
+// .include buscamos el elemento en el arrgelo
+if (num.includes(7)){
+    console.log("SII ESTAAA")
+}
+
+
+// ¿como elevar a la potencia? "**"
+let base = 2;
+let expo = 2;
+let res = base ** expo;
